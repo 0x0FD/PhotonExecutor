e383401060ca1d91e1ac2bddf8f6fccb36fdc65c
FXSploit <imfishercat@gmail.com>
2026-02-21 14:56:30 +0300

    ae

--- notification.lua
+++ notification.lua
@@ -1,4 +1,5 @@
-game.StarterGui:SetCore("SendNotification", {
+local StarterGui = game:GetService("StarterGui")
+StarterGui:SetCore("SendNotification", {
     Title = "Photon",
     Text = "Successfully attached to the game.",
     Duration = 5
