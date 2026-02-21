87ae1894617d46c407abac474d8c99694405a2fa
FXSploit <imfishercat@gmail.com>
2026-02-21 14:58:06 +0300

    Update notification.lua

--- notification.lua
+++ notification.lua
@@ -1,7 +1,7 @@
+print("photon exec v0.3")
 local StarterGui = game:GetService("StarterGui")
 StarterGui:SetCore("SendNotification", {
     Title = "Photon",
     Text = "Successfully attached to the game.",
     Duration = 5
 })
-print("photon exec v0.3")
